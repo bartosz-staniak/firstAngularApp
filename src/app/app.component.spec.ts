e17cb3001991aa301934960f6b15d880cd3b7c7a
bs <bartosz.staniak@gmail.com>
2024-05-28 00:44:33 +0200

    reverted last change after wintessing test fail

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'firstAngularApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firstAngularApp1');
+    expect(app.title).toEqual('firstAngularApp');
   });
 
   it('should render title', () => {
