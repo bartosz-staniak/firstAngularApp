d36f429b32ee5a74b96d03d310247bce215e8513
bs <bartosz.staniak@gmail.com>
2024-05-28 02:53:31 +0200

    added a new test method

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,4 +26,8 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, firstAngularApp');
   });
+
+  it('should have a heading', () => {
+    
+  })
 });
