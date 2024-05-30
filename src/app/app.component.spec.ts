1ee92fb7ef62b0f831bf8a25d082a90d0f4b6b07
bs <bartosz.staniak@gmail.com>
2024-05-30 21:26:18 +0200

    testing toBe expectation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,7 +33,7 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
 
     expect (compiled.querySelector('h2'))
-    .toBeTruthy;
+    .toBe;
   })
 
   it('should have an h2 heading', () => {
