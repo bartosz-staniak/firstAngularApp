2800f167ab4afbd19ce3b5ab59c783ecfe1a9691
bs <bartosz.staniak@gmail.com>
2024-05-30 22:18:22 +0200

    reverted the target element to one that is truly present

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,7 +32,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
 
-    expect (compiled.querySelector('h3')?.hidden).toBe(false);
+    expect (compiled.querySelector('h2')?.hidden).toBe(false);
   })
 
   it('should have an h2 heading', () => {
