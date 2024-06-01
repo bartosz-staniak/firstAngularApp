583aace01aff71e57731d869b007c3c27a1f8b36
bs <bartosz.staniak@gmail.com>
2024-06-01 21:45:35 +0200

    copied a test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -35,6 +35,14 @@ describe('AppComponent', () => {
     expect (compiled.querySelector('h2')?.hidden).toBe(false);
   })
 
+  it('should have an h2 heading present', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect (compiled.querySelector('h2')?.hidden).toBe(false);
+  })
+
   it('should have a paragraph present', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
