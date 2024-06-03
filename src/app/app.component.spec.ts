a674fc5842349a266378bc1e80c619c6de495c05
bs <bartosz.staniak@gmail.com>
2024-06-03 06:21:16 +0200

    removed selection by query

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -40,7 +40,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
 
-    expect (compiled.querySelector("body > app-root > main > div > div.left-side")?.classList.contains("left-side"));
+    expect (compiled.classList.contains("left-side"));
   })
 
   it('should have an h2 heading present', () => {
