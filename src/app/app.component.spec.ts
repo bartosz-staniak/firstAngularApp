cda2bba11ad8a16ff69963b894d8a0df9500d5a8
bs <bartosz.staniak@gmail.com>
2024-06-03 06:05:37 +0200

    swapped checkVisibility with matches

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -40,7 +40,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
 
-    expect (compiled.querySelector("body > app-root > main > div > div.left-side")?.checkVisibility).toBe(true);
+    expect (compiled.querySelector("body > app-root > main > div > div.left-side")?.matches).toBe(true);
   })
 
   it('should have an h2 heading present', () => {
