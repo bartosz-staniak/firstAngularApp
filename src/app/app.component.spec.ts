bea8c7e6b733de080eefb1afcb8e9459a69cd848
bs <bartosz.staniak@gmail.com>
2024-06-03 06:23:42 +0200

    inverted the logical expectation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -40,7 +40,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
 
-    expect (compiled.classList.contains("left-side")).toBe(true);
+    expect (compiled.classList.contains("left-side")).toBe(false);
   })
 
   it('should have an h2 heading present', () => {
