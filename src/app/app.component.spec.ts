3e9a81a4ff3ff4c853f96315c0576ff35b282f9f
bs <bartosz.staniak@gmail.com>
2024-06-05 02:06:58 +0200

    entered an incomplete expexted string to test if it fails

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have the 'Testing a new variable' about variable`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.about).toEqual('failing check');
+    expect(app.about).toEqual('Testing a new');
   })
 
   it('should render title', () => {
