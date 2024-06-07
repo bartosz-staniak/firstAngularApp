ab2aebde6a988e55d4b8038df6659eba34013e61
bs <bartosz.staniak@gmail.com>
2024-06-07 02:23:03 +0200

    changed check's name to reflect its hardcoded expectation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,7 +33,7 @@ describe('AppComponent', () => {
     expect(app.about).toEqual('Testing a new component variable');
   })
 
-  it('should render "about" component property', () => {
+  it('should render "about" component property (static expectation)', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
