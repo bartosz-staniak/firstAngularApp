dd0294aaa991f4f337171db206d1369c50c7914b
bs <bartosz.staniak@gmail.com>
2024-06-07 05:57:17 +0200

    introduced a query selector mistake in the 2nd check

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -54,7 +54,7 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     const app = fixture.componentInstance;
     expect(compiled.querySelector('span')?.textContent).toContain('Testing a new component variable');
-    expect(compiled.querySelector('span')?.textContent).toContain(app.about);
+    expect(compiled.querySelector('h1')?.textContent).toContain(app.about);
   });
 
   it('should have a div present', () => {
