376dc1b6484c2c622eaeb2986b1f68de953f9bb4
bs <bartosz.staniak@gmail.com>
2024-06-07 06:07:14 +0200

    correct the expected string in the first check

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -53,7 +53,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     const app = fixture.componentInstance;
-    expect(compiled.querySelector('span')?.textContent).toContain('Resting a new component variable');
+    expect(compiled.querySelector('span')?.textContent).toContain('Testing a new component variable');
     expect(compiled.querySelector('span')?.textContent).toContain(app.about);
   });
 
