7dd14cf0d7920ef9f731a19fa512b5902b01b103
bs <bartosz.staniak@gmail.com>
2024-06-10 14:34:46 +0200

    added standalone property to remove error from RouterOutlet

--- src/app/snippetsTest.component.ts
+++ src/app/snippetsTest.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root', //was 'selector-name'
+    standalone: true,
     imports: [RouterOutlet],
     templateUrl: 'snippetsTest.component.html'
 })
