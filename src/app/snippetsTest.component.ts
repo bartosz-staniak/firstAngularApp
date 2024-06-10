55e2a7990bb524ee4aa71f3824c05a7aa752e582
bs <bartosz.staniak@gmail.com>
2024-06-10 14:28:58 +0200

    added a comment before the change

--- src/app/snippetsTest.component.ts
+++ src/app/snippetsTest.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'selector-name',
+    selector: 'selector-name', //was 'selector-name'
     templateUrl: 'snippetsTest.component.html'
 })
 
